2d80e613d83f450b5a6a5a3d5f19712ff4213d02
Sam Gould <sjg326@gmail.com>
2018-03-09 08:36:25 -0800

    built simple get endpoint

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
 const {PORT, CLIENT_ORIGIN} = require('./config');
-const {dbConnect} = require('./db-mongoose');
+// const {dbConnect} = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
 );
 
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
+  cors({
+    origin: CLIENT_ORIGIN
+  })
 );
 
+const cat = {
+	imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+	imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+	name: 'Fluffy',
+	sex: 'Female',
+	age: 2,
+	breed: 'Bengal',
+	story: 'Thrown on the street'
+};
+
+app.get('/api/cat', (req, res) => res.json(cat));
+
 function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
-        })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
-        });
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+  // dbConnect();
+  runServer();
 }
 
 module.exports = {app};
